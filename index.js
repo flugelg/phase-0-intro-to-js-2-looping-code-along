d773ea338ebb003d4edb00492de84238e51fbc62
Gary Flugel <flugelg@gmail.com>
2021-12-06 02:59:47 -0500

    commit tree 57bef87ec3ec3b181b65217e99ccc0b2809a8d43

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const arr = [];
+function writeCards(arr, stuff){
+    let ray = [];
+    for (let i = 0; i < arr.length; i++) {
+        ray[i]= `Thank you, ${arr[i]}, for the wonderful ${stuff} gift!`;
+    }
+    return ray;
+}
+
+function countDown(num){
+    let i=0;
+    while (i<=num) {
+        console.log(i++);
+    }
+}
